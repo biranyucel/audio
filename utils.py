9faf0b874d442ae300d44273deab30fef75e21e8
Biran Yucel <biranyucel@gmail.com>
2021-10-06 05:18:36 +0300

    initial commit

---
+++ utils.py
@@ -0,0 +1,54 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import torch
+from IPython.display import Audio, display
+
+#plot_waveform()
+#play_audio()
+
+DEFAULT_OFFSET = 201
+
+[width, height] = matplotlib.rcParams['figure.figsize']
+if width < 10:
+    matplotlib.rcParams['figure.figsize'] = [width * 2.5, height]
+
+def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sample_rate
+
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+    for c in range(num_channels):
+        axes[c].plot(time_axis, waveform[c], linewidth=1)
+        axes[c].grid(True)
+        if num_channels > 1:
+            axes[c].set_ylabel(f'Channel {c+1}')
+        if xlim:
+            axes[c].set_xlim(xlim)
+        if ylim:
+            axes[c].set_ylim(ylim)
+    figure.suptitle(title)
+    plt.show(block=False)
+
+def play_audio(waveform, sample_rate):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    if num_channels == 1:
+        display(Audio(waveform[0], rate=sample_rate))
+    elif num_channels == 2:
+        display(Audio((waveform[0], waveform[1]), rate=sample_rate))
+    else:
+        raise ValueError("Waveform with more than 2 channels are not supported.")
+
+## Demo
+def get_sine_sweep(sample_rate, offset=DEFAULT_OFFSET):
+    max_sweep_rate = sample_rate
+    freq = _get_log_freq(sample_rate, max_sweep_rate, offset)
+    delta = 2 * math.pi * freq / sample_rate
+    cummulative = torch.cumsum(delta, dim=0)
+    signal = torch.sin(cummulative).unsqueeze(dim=0)
+    return signal
